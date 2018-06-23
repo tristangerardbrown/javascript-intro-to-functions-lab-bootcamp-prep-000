2aa5c22e56a4094a9379ad62c43b14ccfbbfbda9
Tristan Brown <tgbrown@stanford.edu>
2018-06-23 19:56:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,27 @@ function logWhisper(string) {
 console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
  if (string.toUpperCase() === string) 
     return "YES INDEED!"
  else if (string === "I love you, Grandma.") 
     return "I love you, too."
 else 
     return "I can't hear you!"
+=======
+  var lowercase = 'hello'
+  lowercase.toLowerCase(string) === lowercase
+    return "I can't hear you!"
+}
+function sayHiToGrandma(string) {
+ var uppercase = 'HELLO!'
+  uppercase.toUpperCase(string) === uppercase
+    return "YES INDEED!"
+}
+function sayHitoGrandma(string) {
+  var mixedCase = 'Hi there!'
+  mixedCase.toLowerCase() === mixedCase 
+  mixedCase.toUpperCase() === mixedCase 
+    return "I love you, too"
+>>>>>>> 15ede42c3917e6be8d4ba2e091cb8f22294044e2
 }
